dc8aaf973c58b7cae542812f7f1b98e3920fbc2a
HUGH <445073309@qq.com>
2017-12-23 22:23:42 +0800

    edit some

--- conversation.py
+++ conversation.py
@@ -1,18 +1,32 @@
 class ConversationMap:
+    conversation_dict = {}
+
+    def __init__(self):
+        self.conversation_dict = {}
+
     @staticmethod
-    def update(self, user, conversation_id):
+    def update(self, user):
         pass
 
     @staticmethod
+    def update(self, user, conversation_id):
+        for key in self.conversation_dict.keys():
+            if key == user.:
+                return self.conversation_dict[key]
+
+    @staticmethod
     def get_conversation(self, username):
-        pass
+        for key in self.conversation_dict.keys():
+            if key == username:
+                return self.conversation_dict[key]
+        return None
 
 
 class Conversation:
     def __init__(self, replier):
         pass
+
 # conversation id
 # user
 # sender
 # receiver
-
